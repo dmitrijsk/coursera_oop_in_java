fdfb265982440d3e76f25efaafc68ba0ee3aa670
me_asus <dmitrijs.kass@gmail.com>
2019-11-20 15:21:00 +0200

    Added comments. Final Quiz passed.

--- 1. Java Programming - Solving Problems with Software/week3/week3_parsing_export_data/Part1.java
+++ 1. Java Programming - Solving Problems with Software/week3/week3_parsing_export_data/Part1.java
@@ -11,16 +11,20 @@ import org.apache.commons.csv.*;
 
 public class Part1 {
     public void tester() {
-        FileResource fr = new FileResource("./exports/exportdata.csv");
+        // Tester creates CSVParser and calls each of the methods below.
+
+        FileResource fr = new FileResource();
 
         CSVParser parser = fr.getCSVParser();     
         System.out.println(countryInfo(parser, "Nauru"));
         
         parser = fr.getCSVParser();
-        listExportersTwoProducts(parser, "gold", "diamonds");
+        listExportersTwoProducts(parser, "cotton", "flowers");
         
         parser = fr.getCSVParser();
-        System.out.println(numberOfExporters(parser, "gold"));
+        String exportItem = "cocoa";
+        System.out.println("Number of exporters of " + 
+                           exportItem + ": " + numberOfExporters(parser, exportItem));
         
         parser = fr.getCSVParser();
         bigExporters(parser,"$999,999,999,999");
@@ -28,6 +32,9 @@ public class Part1 {
     }
 
     public String countryInfo(CSVParser parser, String country) {
+        // This method returns a string of information about the country or 
+        // returns “NOT FOUND” if there is no information about the country.
+        
         String result;
 
         for (CSVRecord record : parser) {
@@ -42,11 +49,11 @@ public class Part1 {
     }
 
     public void listExportersTwoProducts(CSVParser parser, String exportItem1, String exportItem2) {
-        // This method prints the names of all the countries that have both exportItem1 and exportItem2 
-        // as export items. 
-        // For example, using the file exports_small.csv, this method called with the items “gold” and 
-        // “diamonds” would print the countries
+        // This method prints the names of all the countries that have
+        // both exportItem1 and exportItem2 as export items.
 
+        System.out.println("Countries that export both " + exportItem1 + 
+                           " and " + exportItem2 + ":");
         for (CSVRecord record : parser) {
             String currExports = record.get("Exports");
             if (currExports.contains(exportItem1) && currExports.contains(exportItem2)) {
@@ -56,6 +63,8 @@ public class Part1 {
     }
 
     public int numberOfExporters(CSVParser parser, String exportItem) {
+        // This method returns the number of countries that export exportItem.
+        
         int count = 0;
         
         for (CSVRecord record : parser) {
@@ -69,11 +78,15 @@ public class Part1 {
     }
     
     public void bigExporters(CSVParser parser, String amount) {
+        // This method prints the names of countries and their Value amount 
+        // for all countries whose Value (dollars) string is larger than the amount string.
+        
         String currValue;
         for (CSVRecord record : parser) {
             currValue = record.get("Value (dollars)");
             if (currValue.length() > amount.length()) {
-                System.out.println(record.get("Country") + " " + currValue);
+                System.out.println("Big exporters (above " + amount + "): " + 
+                                   record.get("Country") + " " + currValue);
             }
         }
     }
